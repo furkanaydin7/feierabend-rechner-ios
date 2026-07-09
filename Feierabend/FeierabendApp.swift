6b23d83e9f7350a73ca28568ee22f3feb801e880
furkanaydin7 <furkan.aydin@hotmail.ch>
2026-07-09 11:32:03 +0200

    Initial Commit

---
+++ Feierabend/FeierabendApp.swift
@@ -0,0 +1,17 @@
+//
+//  FeierabendApp.swift
+//  Feierabend
+//
+//  Created by Furkan Aydin on 09.07.2026.
+//
+
+import SwiftUI
+
+@main
+struct FeierabendApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
